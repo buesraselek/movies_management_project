2c56061b8976cf35ef5e5d79d0963efcce49efdd
buesraselek <buesra.selek@cloudreach.com>
2022-10-11 15:37:14 +0200

    Updated Backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "ta-terraform-tfstates-535268269273"
-    key    = "sprint1/week2/training-vpc-lab/terraform.tfstates"
+    key    = "movies/movies-projet-exercise/terraform.tfstates"
     dynamodb_table = "terraform-lock"
   }
 }
--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "movie_apps_server" {
   subnet_id              = data.aws_subnet.public_subnet_2.id
   vpc_security_group_ids = [aws_security_group.movie_app_sg2.id]
   key_name = var.keypair_name
-  
+   user_data = "${path.module}/user-data.sh"
 
   tags = {
     Name = "movie-mgmt-server-2"
